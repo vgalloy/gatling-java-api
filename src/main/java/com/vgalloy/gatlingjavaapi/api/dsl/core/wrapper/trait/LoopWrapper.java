03d5fd08c65ca09d279c0f6f64039233f713cd8c
Vincent Galloy <galloy.v@gmail.com>
2020-04-07 21:00:15 +0200

    Apply Apache 2.0 license

--- src/main/java/com/vgalloy/gatlingjavaapi/api/dsl/core/wrapper/trait/LoopWrapper.java
+++ src/main/java/com/vgalloy/gatlingjavaapi/api/dsl/core/wrapper/trait/LoopWrapper.java
@@ -1,9 +1,23 @@
+/*
+ * Copyright 2020 Vincent Galloy
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.vgalloy.gatlingjavaapi.api.dsl.core.wrapper.trait;
 
 import com.vgalloy.gatlingjavaapi.internal.util.expression.Expression;
 import io.gatling.core.structure.ChainBuilder;
 import io.gatling.core.structure.StructureBuilder;
-
 import java.util.Objects;
 import java.util.UUID;
 
@@ -17,15 +31,12 @@ public interface LoopWrapper<
         WRAPPER extends StructureBuilderWrapper<STRUCTURE, WRAPPER>>
     extends ExecsWrapper<STRUCTURE, WRAPPER> {
 
-  default WRAPPER repeat(
-      final int times, final ExecsWrapper<ChainBuilder, ?> execsWrapper) {
+  default WRAPPER repeat(final int times, final ExecsWrapper<ChainBuilder, ?> execsWrapper) {
     return repeat(times, UUID.randomUUID().toString(), execsWrapper);
   }
 
   default WRAPPER repeat(
-      final int times,
-      final String counterName,
-      final ExecsWrapper<ChainBuilder, ?> execsWrapper) {
+      final int times, final String counterName, final ExecsWrapper<ChainBuilder, ?> execsWrapper) {
     Objects.requireNonNull(counterName, "counterName");
     Objects.requireNonNull(execsWrapper, "execsWrapper");
 
